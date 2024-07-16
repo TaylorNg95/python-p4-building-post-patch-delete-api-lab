48516654c2917c44719b6c05f1ecd00ced292a4e
Taylor Ng <taylormng95@gmail.com>
2024-07-16 11:44:25 -0400

    completes lab

--- server/app.py
+++ server/app.py
@@ -23,12 +23,54 @@ def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
     return make_response(  bakeries,   200  )
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET', 'PATCH'])
 def bakery_by_id(id):
-
     bakery = Bakery.query.filter_by(id=id).first()
     bakery_serialized = bakery.to_dict()
-    return make_response ( bakery_serialized, 200  )
+    
+    if request.method == 'GET':
+        return make_response ( bakery_serialized, 200  )
+    
+    elif request.method == 'PATCH':
+        bakery.name = request.form.get('name')
+        db.session.add(bakery)
+        db.session.commit()
+
+        return make_response(bakery.to_dict(), 200)
+    
+@app.route('/baked_goods', methods=['GET', 'POST'])
+def baked_goods():
+    baked_goods = [bakery.to_dict() for bakery in Bakery.query.all()]
+    
+    if request.method == 'GET':
+        return make_response(  baked_goods,   200  )
+    
+    elif request.method == 'POST':
+        new_baked_good = BakedGood(
+            name = request.form.get('name'),
+            price = request.form.get('price'),
+            bakery_id = request.form.get('bakery_id'),
+        )
+
+        db.session.add(new_baked_good)
+        db.session.commit()
+
+        return make_response(new_baked_good.to_dict(), 201)
+    
+@app.route('/baked_goods/<int:id>', methods=['GET', 'DELETE'])
+def baked_goods_by_id(id):
+    baked_good = BakedGood.query.filter(BakedGood.id == id).first()
+
+    if request.method == 'GET':
+        return make_response(baked_good.to_dict(), 200)
+    
+    elif request.method == 'DELETE':
+        db.session.delete(baked_good)
+        db.session.commit()
+        response_body = {
+            'message': 'record successfully deleted'
+        }
+        return make_response(response_body, 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
